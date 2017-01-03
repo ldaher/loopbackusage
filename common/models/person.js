0f2812f65bcca35180e38e991395bc06c0ccb580
Luciano Daher <luciano.daher@gmail.com>
2017-01-03 09:21:01 -0200

    introduced remoteMethod successfully

--- common/models/person.js
+++ common/models/person.js
@@ -1,8 +1,17 @@
 'use strict';
 
 module.exports = function (Person) {
-    Person.getPerson = function (req, res, next) {
+    Person.getPerson = function (req, res, cb) {
         console.log(req);
+        res.end("Hello  Persons!");
     };
 
+    Person.remoteMethod('getPerson', {
+        accepts: [{ arg: 'req', type: 'object', 'http': { source: 'req' } },
+        { arg: 'res', type: 'object', 'http': { source: 'res' } }],
+        http: {
+            verb: 'get',
+            path: '/greet'
+        }
+    });
 };
--- server/middleware/bodyParsing.js
+++ server/middleware/bodyParsing.js
@@ -1,6 +1,10 @@
 module.exports = function () {
     return function bodyParsing(req, res, next) {
         console.log("Printing out req added property: " + req.surName);
-        res.write("Luciano " + req.surName);
+        if(req.url === '/greet'){
+            next();
+            return;
+        }
+        res.end("Luciano " + req.surName);
     }
 }
